88679f74f5d84c3fc04f9f38b550f1f1d6275c74
Phuchirita <phuchirita.b@gmail.com>
2021-12-19 15:13:44 +0700

    Test push git

--- todolist.php
+++ todolist.php
@@ -1,30 +1,20 @@
-<?php 
-
-$con = mysqli_connect("localhost","root","Suchada-pp-77","todolist");
-
-$sql = "select * from list";
-
-$data = $con->query($sql);
-?>
 
 <html>
 <head>Todo List</head>
 <body>
     <div>
         <form >
-            <input type="text" name="list" require><button type="submit">ADD</button>
+            <input type="text" name="list" require><button type="submit">add</button>
             <table>
-                <?php while($result = $data->fetch_assoc()): ?>
                 <tr>
                     <td><input type="checkbox" name="check"></td>
-                    <td><?php echo $result['ListName']; ?></td>
-                    <td><?php echo $result['Date']; ?></td>
+                    <td></td>
+                    <td></td>
                     <td>
                         <button type="submit">Update</button>
                         <button type="submit">Delete</button>
                     </td>
                 </tr>
-                <?php endwhile ?>
             </table>
         </form>
     </div>
